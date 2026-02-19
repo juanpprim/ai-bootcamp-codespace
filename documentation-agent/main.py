37afcecce59cabf8ad8f50111fc085e08b087471
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2026-02-19 14:06:48 +0100

    streaming

--- documentation-agent/main.py
+++ documentation-agent/main.py
@@ -1,10 +1,33 @@
 from pydantic_ai import Agent, RunUsage
 
-from doc_agent import DocumentationAgentConfig, create_agent, run_agent
+from doc_agent import (
+    DocumentationAgentConfig,
+    create_agent,
+    run_agent,
+    run_agent_stream,
+    DEFAULT_INSTRUCTIONS
+)
+
 from tools import create_documentation_tools_cached
 
 import dotenv
 dotenv.load_dotenv()
+
+
+def print_messages(messages):
+    for m in messages:
+        print(m.kind)
+        for p in m.parts:
+            part_kind = p.part_kind
+            if part_kind == 'user-prompt':
+                print('  USER:', p.content)
+            if part_kind == 'tool-call':
+                print('  TOOL CALL:', p.tool_name, p.args)
+            if part_kind == 'tool-return':
+                print('  TOOL RETURN:', p.tool_name)
+            if part_kind == 'text':
+                print('  ASSISTANT:', p.content)
+        print()
 
 
 async def run_qna(agent: Agent):
@@ -26,14 +49,16 @@ async def run_qna(agent: Agent):
 async def run_agent_question():
     user_prompt = "LLM as a judge"
     print(f"Running agent with question: {user_prompt}...")
-    
+
     tools = create_documentation_tools_cached()
-    agent_config = DocumentationAgentConfig()
+    agent_config = DocumentationAgentConfig(
+        instructions=DEFAULT_INSTRUCTIONS
+    )
 
     agent = create_agent(agent_config, tools)
 
-    result = await run_agent(agent, user_prompt)
-    print(result.output)
+    result = await run_agent_stream(agent, user_prompt)
+    print_messages(result.new_messages())
 
 
 if __name__ == "__main__":
